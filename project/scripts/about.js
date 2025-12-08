e0e5d56bcb932ca26ca7b41429acd1c915112698
juliusmiguelditan1 <jditan@byupathway.edu>
2025-12-08 21:17:08 +0800

    second to the last

--- project/scripts/about.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-      const navLinks = document.getElementById('navLinks');
-      navLinks.classList.toggle('show');
-    }
-
-    // Footer last modified
-    const full = document.querySelector("#full");
-    const today = new Date(document.lastModified);
-    full.innerHTML = "Last Modified: " + today.toLocaleString();
